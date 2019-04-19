ea7e3e1aa10d7e5acc438c491fe4a313b42a4cb5
Dario L Garcia <dario.garcia@10pines.com>
2019-04-19 18:58:29 -0300

    Agregar config y servicio de ruteo de requests

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
